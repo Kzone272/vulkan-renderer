c9f05a1feec418092619a1cb83b5bdda1a0476ee
Kevin Haslett <kevinahaslett@gmail.com>
2023-01-29 23:43:07 -0500

    Added SDL2 and created a simple window
    Also stopped using FetchContent because it was being troublesome.
    I've installed packages with vcpkg on Windows and that works fine.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,45 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+#undef main
+
 #include "utils.h"
 
-void main() {
+int main() {
   utils::PrintMatrix();
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cout << "Failed to initialize the SDL2 library\n";
+    return -1;
+  }
+
+  SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                        SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED,
+                                        680, 480,
+                                        SDL_WINDOW_RESIZABLE);
+  if (!window) {
+    std::cout << "Failed to create window\n";
+    return -1;
+  }
+
+  SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+  if (!window_surface) {
+    std::cout << "Failed to get the surface from the window\n";
+    return -1;
+  }
+  SDL_UpdateWindowSurface(window);
+
+  SDL_Event event;
+  while (true) {
+    SDL_PollEvent(&event);
+    if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE) {
+      break;
+    }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
