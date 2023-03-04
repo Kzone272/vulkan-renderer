3ac218471b9e862c85b89ca03ed4730978742607
Kevin Haslett <kevinahaslett@gmail.com>
2023-03-04 00:37:04 -0500

    Print validation layer messages

--- src/hello_triangle_app.h
+++ src/hello_triangle_app.h
@@ -16,8 +16,21 @@ using std::cout;
 using std::endl;
 using std::printf;
 
+namespace {
+
 constexpr int WIDTH = 800;
 constexpr int HEIGHT = 600;
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL
+debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT msg_severity,
+              VkDebugUtilsMessageTypeFlagsEXT msg_type,
+              const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+              void* user_data) {
+  printf("validation layer: %s\n", callback_data->pMessage);
+  return VK_FALSE;
+}
+
+}  // namespace
 
 class HelloTriangleApp {
  public:
@@ -45,6 +58,9 @@ class HelloTriangleApp {
 
   void initVulkan() {
     createInstance();
+    if (enable_validation_layers_) {
+      setupDebugMessenger();
+    }
   }
 
   void mainLoop() {
@@ -69,23 +85,33 @@ class HelloTriangleApp {
     app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     app_info.apiVersion = VK_API_VERSION_1_0;
 
-    VkInstanceCreateInfo create_info{};
-    create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    create_info.pApplicationInfo = &app_info;
+    VkInstanceCreateInfo instance_ci{};
+    instance_ci.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instance_ci.pApplicationInfo = &app_info;
 
     auto ext_names = getRequiredExtensions();
-    create_info.enabledExtensionCount = ext_names.size();
-    create_info.ppEnabledExtensionNames = ext_names.data();
+    instance_ci.enabledExtensionCount = ext_names.size();
+    instance_ci.ppEnabledExtensionNames = ext_names.data();
 
     auto validation_layers = getValidationLayers();
-    create_info.enabledLayerCount = validation_layers.size();
-    create_info.ppEnabledLayerNames = validation_layers.data();
+    if (validation_layers.size()) {
+      instance_ci.enabledLayerCount = validation_layers.size();
+      instance_ci.ppEnabledLayerNames = validation_layers.data();
+    } else {
+      instance_ci.enabledLayerCount = 0;
+    }
+
+    VkDebugUtilsMessengerCreateInfoEXT dbg_messenger_ci{};
+    if (enable_validation_layers_) {
+      makeDbgMessengerCi(dbg_messenger_ci);
+      instance_ci.pNext = &dbg_messenger_ci;
+    }
 
 #if __APPLE__
-    create_info.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+    instance_ci.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 #endif
 
-    VKASSERT(vkCreateInstance(&create_info, nullptr, &instance_));
+    VKASSERT(vkCreateInstance(&instance_ci, nullptr, &instance_));
   }
 
   std::vector<const char*> getRequiredExtensions() {
@@ -99,7 +125,11 @@ class HelloTriangleApp {
     ext_names.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
 #endif
 
-    printf("Required extensions (%lu):\n", ext_names.size());
+    if (enable_validation_layers_) {
+      ext_names.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    printf("Required extensions (%zu):\n", ext_names.size());
     for (auto& name : ext_names) {
       printf("  %s\n", name);
     }
@@ -122,9 +152,9 @@ class HelloTriangleApp {
 
   std::vector<const char*> getValidationLayers() {
     std::vector<const char*> required_layers;
-#ifdef DEBUG
-    required_layers.push_back("VK_LAYER_KHRONOS_validation");
-#endif
+    if (enable_validation_layers_) {
+      required_layers.push_back("VK_LAYER_KHRONOS_validation");
+    }
 
     if (required_layers.size()) {
       uint32_t layer_count = 0;
@@ -153,10 +183,41 @@ class HelloTriangleApp {
       }
     }
 
+    printf("Required layers (%zu):\n", required_layers.size());
+    for (const auto& layer : required_layers) {
+      printf("  %s\n", layer);
+    }
+
     return required_layers;
   }
 
+#define LOAD_VK_FN(fn) (PFN_##fn) vkGetInstanceProcAddr(instance_, #fn);
+
+  void makeDbgMessengerCi(VkDebugUtilsMessengerCreateInfoEXT& ci) {
+    ci.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    ci.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                         VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    ci.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                     VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                     VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    ci.pfnUserCallback = debugCallback;
+    ci.pUserData = nullptr;
+  }
+
+  void setupDebugMessenger() {
+    VkDebugUtilsMessengerCreateInfoEXT ci{};
+    makeDbgMessengerCi(ci);
+    auto create_fn = LOAD_VK_FN(vkCreateDebugUtilsMessengerEXT);
+    ASSERT(create_fn);
+    VKASSERT(create_fn(instance_, &ci, nullptr, &dbg_messenger_));
+  }
+
   void cleanup() {
+    if (enable_validation_layers_) {
+      auto destroy_dbg_fn = LOAD_VK_FN(vkDestroyDebugUtilsMessengerEXT);
+      ASSERT(destroy_dbg_fn);
+      destroy_dbg_fn(instance_, dbg_messenger_, nullptr);
+    }
     vkDestroyInstance(instance_, nullptr);
 
     SDL_DestroyWindow(window_);
@@ -166,4 +227,11 @@ class HelloTriangleApp {
   SDL_Window* window_ = nullptr;
 
   VkInstance instance_;
+  VkDebugUtilsMessengerEXT dbg_messenger_;
+
+#ifdef DEBUG
+  const bool enable_validation_layers_ = true;
+#else
+  const bool enable_validation_layers_ = false;
+#endif  // DEBUG
 };
