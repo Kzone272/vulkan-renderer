9aa858459adb4795e9a82ff6a68d5bb91616bd38
Kevin Haslett <kevinahaslett@gmail.com>
2023-06-23 00:53:46 -0400

    Separate rendering code from window and animation code
    The line around window resizing is still messier than I'd like it.

---
+++ src/frame-state.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_LEFT_HANDED
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+struct UniformBufferObject {
+  alignas(16) glm::mat4 model;
+  alignas(16) glm::mat4 view;
+  alignas(16) glm::mat4 proj;
+};
+
+struct AnimationState {
+  float clear_val = 0.0f;
+  float model_rot = 0.0f;
+};
+
+struct FrameState {
+  uint64_t frame_num = 0;
+  AnimationState anim;
+  UniformBufferObject ubo;
+
+  uint32_t width = 1;
+  uint32_t height = 1;
+  bool window_resized = false;
+  bool empty_window = false;
+};
