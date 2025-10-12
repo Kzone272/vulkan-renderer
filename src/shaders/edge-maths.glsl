352c34d68b0861cc38f8d33280b0b13c4ee72df1
Kevin Haslett <kevinahaslett@gmail.com>
2025-10-12 14:35:03 -0400

    Fix bug preventing some edges
    When the normal angle diff was greater than 90 degrees it cold prevent edge detection

--- src/shaders/edge-maths.glsl
+++ src/shaders/edge-maths.glsl
@@ -3,7 +3,7 @@
 bool edgeBetween(
     vec3 aNorm, vec3 aPos, vec3 bNorm, vec3 bPos,
     float depth_thresh, float angle_thresh) {
-  float cosang = abs(dot(aNorm, bNorm));
+  float cosang = dot(aNorm, bNorm);
   float plane_d = abs(dot(aNorm, bPos - aPos));
 
   return cosang < angle_thresh || plane_d > depth_thresh;
